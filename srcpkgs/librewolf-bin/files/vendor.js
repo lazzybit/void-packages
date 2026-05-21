70cda778949428090636099f6183337bc154f00d
Riley Chang <riley@nomonator.com>
2026-05-21 23:59:57 +0800

    New package: librewolf-bin-150.0.3

---
+++ srcpkgs/librewolf-bin/files/vendor.js
@@ -0,0 +1,6 @@
+// Use system-provided dictionaries
+pref("spellchecker.dictionary_path", "/usr/share/hunspell");
+
+// Don't disable extensions in the application directory
+// done in librewolf.cfg
+// pref("extensions.autoDisableScopes", 11);
